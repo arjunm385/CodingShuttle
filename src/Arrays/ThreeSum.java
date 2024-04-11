cd2c514fa67cdc2e646968af750833516ba068d9
arjun <arjunm385@gmail.com>
2024-04-11 18:42:22 +0530

    Added ThreeSum.java completed

--- src/Arrays/ThreeSum.java
+++ src/Arrays/ThreeSum.java
@@ -8,41 +8,48 @@ public class ThreeSum {
 
     public static void main(String[] args) {
 
-        int[] arr = {2,7,11,15,2,7};
-        int t = 9;
+        int[] arr = {-1, 0, 1, 2, -1, -4};
 
         Arrays.sort(arr);
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
 
-        getPairs(arr, t, 0, arr.length-1, list);
+        for (int i = 0; i < arr.length - 2; i++) {
 
-        System.out.println(list);
+            if (i > 0 && arr[i] == arr[i - 1])
+                continue;
+            List<List<Integer>> list = new ArrayList<>();
+            getPairs(arr, -arr[i], i + 1, arr.length - 1, list);
+            if (!list.isEmpty()) {
+                res.addAll(list);
+            }
+        }
 
+        System.out.println(res);
     }
 
     static void getPairs(int[] a, int t, int l, int r, List<List<Integer>> list) {
 
-        while(l<r) {
+        int start = l;
 
-            if(l>0 && a[l] == a[l-1]) {
+        while (l < r) {
+
+            if (l > start && a[l] == a[l - 1]) {
                 l++;
                 continue;
             }
 
-            if(r<a.length-1 && a[r] == a[r+1]) {
+            if (r < a.length - 1 && a[r] == a[r + 1]) {
                 r--;
                 continue;
             }
 
-            if(a[l] + a[r] < t) {
+            if (a[l] + a[r] < t) {
                 l++;
-            }
-            else if(a[l] + a[r] > t) {
+            } else if (a[l] + a[r] > t) {
                 r--;
-            }
-
-            else {
+            } else {
                 List<Integer> temp = new ArrayList<>();
+                temp.add(-t);
                 temp.add(a[l]);
                 temp.add(a[r]);
                 list.add(temp);
